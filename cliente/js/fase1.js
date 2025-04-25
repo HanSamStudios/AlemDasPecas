26d1201b6c287b9271685749df15d9ba7909a80e
hanadaxz <gabriel.e2005@aluno.ifsc.edu.br>
2025-04-25 13:46:46 +0000

    andando

--- cliente/js/fase1.js
+++ cliente/js/fase1.js
@@ -2,6 +2,9 @@ export default class fase1 extends Phaser.Scene {
 
   constructor () {
     super('fase1')
+
+    this.threshold = 0.2
+    this.speed = 125
   }
 
   init () { }
@@ -22,17 +25,24 @@ export default class fase1 extends Phaser.Scene {
       frameWidth: 64,
       frameHeight: 64
     })
+    this.load.plugin(
+      'rexvirtualjoystickplugin',
+      'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexvirtualjoystickplugin.min.js',
+      true
+    )
   }
 
   create () {
-    this.cameras.main.setBackgroundColor('#87ceeb');
+    this.cameras.main.setBackgroundColor('#87ceeb')
+
     // mapa
     this.tilemapMapa = this.make.tilemap({ key: 'mapa' })
+
     // tilesets
     this.tilesetArvore = this.tilemapMapa.addTilesetImage('arvore', null, 256, 208)
     this.tilesetChao = this.tilemapMapa.addTilesetImage('chao', null, 64, 64)
     this.tilesetVaso = this.tilemapMapa.addTilesetImage('vaso', null, 64, 64)
-    this.tilesetFlores = this.tilemapMapa.addTilesetImage('flores', null, 64,32)
+    this.tilesetFlores = this.tilemapMapa.addTilesetImage('flores', null, 64, 32)
 
     // camadas
     this.layerChao = this.tilemapMapa.createLayer('chao', [this.tilesetChao])
@@ -42,43 +52,109 @@ export default class fase1 extends Phaser.Scene {
     const spawnPoint = this.tilemapMapa.findObject("spawn", obj => obj.name === "spawn")
 
     // personagens
-    this.fox = this.physics.add.sprite(spawnPoint.x, spawnPoint.y, 'fox')
-    this.cameras.main.startFollow(this.fox)
+    this.personagemLocal = this.physics.add.sprite(spawnPoint.x, spawnPoint.y, 'fox')
+    this.cameras.main.startFollow(this.personagemLocal)
 
     // colisao
     this.layerChao.setCollisionByProperty({ collides: true })
-    this.physics.add.collider(this.fox, this.layerChao, this.bomba)
+    this.physics.add.collider(this.personagemLocal, this.layerChao)
 
-    //animacao
+    // animações
     this.anims.create({
-      key: 'fox-direita',
+      key: 'personagem-andando-direita',
       frames: this.anims.generateFrameNumbers('fox', { start: 3, end: 9 }),
       frameRate: 10,
       repeat: -1
     })
     this.anims.create({
+      key: 'personagem-andando-esquerda',
+      frames: this.anims.generateFrameNumbers('fox', { start: 3, end: 9 }),
+      frameRate: 10,
+      repeat: -1
+    })
+
+    this.anims.create({
+      key: 'personagem-parado-direita',
+      frames: this.anims.generateFrameNumbers('fox', { start: 1, end: 1 }),
+      frameRate: 1
+    })
+
+    this.anims.create({
       key: 'bomba',
       frames: this.anims.generateFrameNumbers('bomba', { start: 0, end: 6 }),
       frameRate: 10
     })
-    // personagem
-    this.fox.play('fox-direita')
-    this.fox.setVelocityX(100)
 
     // bomba
     this.bomba = this.physics.add.sprite(400, 225, 'bomba')
-    this.bomba.body.setGravity(0, 0)
     this.bomba.body.setAllowGravity(false)
-    
-    this.bomba
-      .setInteractive()
-      .on('pointerdown', () => {
-        this.bomba.play('bomba')
 
-      
-      })
+    this.bomba.setInteractive().on('pointerdown', () => {
+      this.bomba.play('bomba')
+    })
+
+    // joystick
+    this.joystick = this.plugins.get('rexvirtualjoystickplugin').add(this, {
+      x: 200,
+      y: 310,
+      radius: 50,
+      base: this.add.circle(120, 360, 50, 0x888888),
+      thumb: this.add.circle(120, 360, 25, 0xcccccc)
+    })
   }
 
-  update () { }
+    update() {
+      const angle = Phaser.Math.DegToRad(this.joystick.angle) // Converte o ângulo para radianos
+      const force = this.joystick.force
 
-}
+      if (force > this.threshold) {
+        const velocityX = Math.cos(angle) * this.speed
+        const velocityY = Math.sin(angle) * this.speed
+
+        this.personagemLocal.setVelocity(velocityX, velocityY)
+
+        // Animação do personagem conforme a direção do movimento
+        if (Math.abs(velocityX) > Math.abs(velocityY)) {
+          if (velocityX > 0) {
+            this.personagemLocal.setFlipX(false)
+            this.personagemLocal.anims.play('personagem-andando-direita', true)
+            this.direcaoAtual = 'direita'
+          } else {
+            this.personagemLocal.setFlipX(true)
+            this.personagemLocal.anims.play('personagem-andando-esquerda', true)
+            this.direcaoAtual = 'esquerda'
+          }
+        } else {
+          if (velocityY > 0) {
+            this.personagemLocal.anims.play('personagem-andando-frente', true)
+            this.direcaoAtual = 'frente'
+          } else {
+            this.personagemLocal.anims.play('personagem-andando-tras', true)
+            this.direcaoAtual = 'tras'
+          }
+        }
+      } else {
+        // Se a força do joystick for baixa, o personagem para
+        this.personagemLocal.setVelocity(0)
+
+        switch (this.direcaoAtual) {
+          case 'frente':
+            this.personagemLocal.anims.play('personagem-parado-frente', true)
+            break
+          case 'direita':
+            this.personagemLocal.anims.play('personagem-parado-direita', true)
+            break
+          case 'esquerda':
+            this.personagemLocal.anims.play('personagem-parado-esquerda', true)
+            break
+          case 'tras':
+            this.personagemLocal.anims.play('personagem-parado-tras', true)
+            break
+        }
+      }
+    }
+  }
+
+
+
+
