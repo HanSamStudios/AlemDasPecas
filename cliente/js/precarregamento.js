5ecf46bc3959e7ff35cf4980a37df2c43a333984
hanadaxz <gabriel.e2005@aluno.ifsc.edu.br>
2025-06-06 08:40:49 -0300

    precarregamento

--- cliente/js/precarregamento.js
+++ cliente/js/precarregamento.js
@@ -14,45 +14,82 @@ export default class precarregamento extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("chao", "assets/mapa/chao.png");
+    this.load.tilemapTiledJSON("mapa", "assets/mapa/mapa.json");
     this.load.image("arvore", "assets/mapa/arvore.png");
+    this.load.image("tp", "assets/mapa/tp.png");
+    this.load.image("objetivo", "assets/objetivo.png");
+    this.load.image("chao", "assets/mapa/chao.png");
+    this.load.image("fantasm", "assets/mapa/fantasm.png");
+    this.load.image("placaa", "assets/mapa/placaa.png");
     this.load.image("flores", "assets/mapa/flores.png");
-    this.load.image("vaso", "assets/mapa/vaso.png");
-    this.load.audio("horror", "assets/horror.mp3");
+    this.load.image("jump", "assets/jump.png");
+    this.load.image("cemiterio", "assets/cemiterio.png");
+    this.load.image("plataforma", "assets/plataforma.png");
+    this.load.image("dash", "assets/dash.png");
+    this.load.image("fundo2", "assets/fundo2.png");
+    this.load.image("coracao", "assets/coracao.png");
+    this.load.image("casa", "assets/mapa/casa.png");
     this.load.image("back", "assets/parallax/back.png");
     this.load.image("repeat", "assets/repeat.png");
+    this.load.image("vaso", "assets/mapa/vaso.png");
     this.load.image("espinhos", "assets/mapa/espinhos.png");
-    this.load.setPath("assets/");
-    this.load.image("jump", "jump.png");
-    this.load.image("fundo2", "fundo2.png");
-    this.load.image("capa", "capa.png");
-    this.load.image("fceu", "ceu.jpg");
-    this.load.audio("musica", "musica.mp3");
-    this.load.audio("jumpsound", "jumpsound.mp3");
-    this.load.audio("dashsound", "dashsound.mp3");
-    this.load.audio("crystalsound", "crystalsound.mp3");
-    this.load.plugin(
-      "rexvirtualjoystickplugin",
-      "https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexvirtualjoystickplugin.min.js",
-      true
-    );
-    this.load.audio("morte", "morte.mp3");
-    this.load.spritesheet("crystal", "greencrystal.png", {
+    this.load.spritesheet("foxmalmorte", "assets/foxmalmorte.png", {
       frameWidth: 64,
       frameHeight: 64,
     });
-    this.load.spritesheet("fox-laranja", "Spritesheet.png", {
+    this.load.spritesheet("flag", "assets/flag.png", {
       frameWidth: 64,
       frameHeight: 64,
     });
-    this.load.spritesheet("fox-roxo", "foxroxo.png", {
+    this.load.spritesheet("crystal", "assets/greencrystal.png", {
       frameWidth: 64,
       frameHeight: 64,
     });
-    this.load.spritesheet("bomba", "bomba.png", {
+    this.load.spritesheet("dano", "assets/perde.png", {
+      frameWidth: 64,
+      frameHeight: 64,
+    });
+    this.load.spritesheet("passarinho", "assets/passarinho.png", {
+      frameWidth: 64,
+      frameHeight: 64,
+    });
+    this.load.spritesheet("passarinho-dano", "assets/passarinho-dano.png", {
+      frameWidth: 64,
+      frameHeight: 64,
+    });
+    this.load.audio("musicaa", "assets/musicaa.mp3");
+    this.load.audio("horror", "assets/horror.mp3");
+    this.load.audio("fantasma", "assets/fantasma.mp3");
+    this.load.audio("dashsound", "assets/dashsound.mp3");
+    this.load.audio("crystalsound", "assets/crystalsound.mp3");
+    this.load.audio("jumpsound", "assets/jumpsound.mp3");
+    this.load.audio("morte", "assets/morte.mp3");
+      this.load.audio("passarosound", "assets/passarosound.mp3");
+    this.load.image("fceu", "assets/ceu.jpg");
+    this.load.spritesheet("bomba", "assets/mapa/bomba.png", {
       frameWidth: 8,
       frameHeight: 8,
     });
+    
+    // personagens (colocar o segundo depois)
+    this.load.spritesheet("fox-primeiro", "assets/Spritesheet.png", {
+      frameWidth: 64,
+      frameHeight: 64,
+    });
+    this.load.spritesheet("fox-segundo", "assets/foxroxo.png", {
+      frameWidth: 64,
+      frameHeight: 64,
+    });
+    this.load.spritesheet("foxmal", "assets/foxmal.png", {
+      frameWidth: 64,
+      frameHeight: 64,
+    });
+    this.load.plugin(
+      "rexvirtualjoystickplugin",
+      "./js/rexvirtualjoystickplugin.min.js",
+      true
+    );
+    this.load.image("fullscreen", "assets/fullscreen.png");
   }
 
   create() {
