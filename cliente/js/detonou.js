304e4e50cba22fb8508e410686951f4c1b427364
hanadaxz <gabriel.e2005@aluno.ifsc.edu.br>
2025-06-06 08:36:16 -0300

    operacao de credito 2

--- cliente/js/detonou.js
+++ cliente/js/detonou.js
@@ -8,6 +8,43 @@ export default class Detonou extends Phaser.Scene {
   }
 
   create() {
+        globalThis.google.accounts.id.initialize({
+        client_id:
+          "331191695151-ku8mdhd76pc2k36itas8lm722krn0u64.apps.googleusercontent.com",
+        callback: (res) => {
+          if (res.error) {
+            console.error(res.error);
+          } else {
+            axios
+              .post(
+                "https://feira-de-jogos.dev.br/api/v2/credit",
+                {
+                  product: 41, // id do jogo cadastrado no banco de dados da Feira de Jogos
+                  value: 400, // crédito em tijolinhos
+                },
+                {
+                  headers: {
+                    Authorization: `Bearer ${res.credential}`,
+                  }
+                }
+              )
+              .then(function (response) {
+                console.log(response);
+                alert("Crédito adicionado!")
+              })
+              .catch(function (error) {
+                console.error(error)
+                alert("erro ao adicionar crédito: (")
+              });
+          }
+        },
+      });
+  
+      globalThis.google.accounts.id.prompt((notification) => {
+        if (notification.isNotDisplayed() || notification.isSkippedMoment()) {
+          globalThis.google.accounts.id.prompt();
+        }
+      });
     this.add.image(0, 0, 'detonou').setOrigin(0);
   }
 }
