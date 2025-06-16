b96b91a03a367f22d3d2790f54b42978113a0ce3
hanadaxz <gabriel.e2005@aluno.ifsc.edu.br>
2025-06-16 15:37:14 -0300

    uwe

--- cliente/js/detonou.js
+++ cliente/js/detonou.js
@@ -7,51 +7,46 @@ export default class Detonou extends Phaser.Scene {
     this.load.image('detonou', 'assets/detonou.png'); // Adicione essa imagem na pasta
   }
 
-create() {
-  globalThis.google.accounts.id.initialize({
-    client_id: "331191695151-ku8mdhd76pc2k36itas8lm722krn0u64.apps.googleusercontent.com",
-    callback: (res) => {
-      console.log("Resposta do login Google:", res);
-
-      if (res.error) {
-        console.error("Erro no login:", res.error);
-        return;
-      }
-
-      const token = res.credential;
-      console.log("Token recebido:", token);
-
-      axios.post(
-        "https://feira-de-jogos.dev.br/api/v2/credit",
-        {
-          product: 41,
-          value: 1500,
+  create() {
+        globalThis.google.accounts.id.initialize({
+        client_id:
+          "331191695151-ku8mdhd76pc2k36itas8lm722krn0u64.apps.googleusercontent.com",
+        callback: (res) => {
+          console.log("Resposta do login Google:", res);
+          if (res.error) {
+            console.error(res.error);
+          } else {
+            axios
+              .post(
+                "https://feira-de-jogos.dev.br/api/v2/credit",
+                {
+                  product: 41, // id do jogo cadastrado no banco de dados da Feira de Jogos
+                  value: 1500, // crédito em tijolinhos
+                },
+                {
+                  headers: {
+                    Authorization: `Bearer ${res.credential}`,
+                  }
+                }
+              )
+              .then(function (response) {
+                console.log(response);
+                alert("Crédito adicionado!")
+              })
+              .catch(function (error) {
+                console.error(error)
+                alert("erro ao adicionar crédito :(")
+              });
+          }
         },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      )
-      .then((response) => {
-        console.log("Crédito adicionado com sucesso:", response.data);
-        alert("Crédito adicionado!");
-      })
-      .catch((error) => {
-        console.error("Erro ao adicionar crédito:", error.response?.data || error.message || error);
-        alert("Erro ao adicionar crédito :(");
       });
-
-      // Exibe a imagem da cena normalmente
-      this.add.image(0, 0, 'detonou').setOrigin(0);
-    },
-  });
-
-  globalThis.google.accounts.id.prompt((notification) => {
-    if (notification.isNotDisplayed() || notification.isSkippedMoment()) {
-      globalThis.google.accounts.id.prompt();
-    }
-  });
-}
-
+  
+      globalThis.google.accounts.id.prompt((notification) => {
+        if (notification.isNotDisplayed() || notification.isSkippedMoment()) {
+          globalThis.google.accounts.id.prompt();
+        }
+      });
+    this.add.image(0, 0, 'detonou').setOrigin(0);
+  }
+  
 }
