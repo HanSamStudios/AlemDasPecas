177f9d16f84062936c77e8838dd3144270b312ce
hanadaxz <gabriel.e2005@aluno.ifsc.edu.br>
2025-06-17 12:32:13 -0300

    delete

--- cliente/js/index.js
+++ cliente/js/index.js
@@ -5,9 +5,7 @@ import abertura from "./abertura.js";
 import precarregamento from "./precarregamento.js";
 import sala from "./sala.js";
 import fase1 from "./fase1.js";
-import finalacabado from "./final-acabado.js";
 import finalperdeu from "./final-perdeu.js";
-import detonou from "./detonou.js";
 import finalunico from "./final-unico.js";
 
 class Game extends Phaser.Game {
@@ -35,9 +33,7 @@ class Game extends Phaser.Game {
     this.scene.add("precarregamento", precarregamento);
     this.scene.add("sala", sala);
     this.scene.add("fase1", fase1);
-    this.scene.add("finalacabado", finalacabado);
     this.scene.add("finalperdeu", finalperdeu);
-    this.scene.add("detonou", detonou);
     this.scene.add("finalunico", finalunico);
     this.scene.start("abertura");
   }
